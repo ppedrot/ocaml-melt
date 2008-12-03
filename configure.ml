79f758dd97140ca57fdd3cf714daa9c0ba5f76a7
darcs <darcs@doomeer.com>
2008-12-03 15:19:30 +0000

    configure: interface seems cool

--- configure.ml
+++ configure.ml
@@ -1,4 +1,4 @@
-(*******************************************************************************)
+(******************************************************************************)
 open Printf
 
 module Version: sig
@@ -7,20 +7,35 @@ module Version: sig
   val of_string: string -> t
   val to_string: t -> string
   val compare: t -> t -> int
+  val max: t -> t -> t
 end = struct
   type t = int list * string
 
   exception Uncomparable_versions of t * t
 
   let of_string s =
-    let p = ref 0 in
-    let v = ref [] in
-    begin try while true do
-      let i = String.index_from s !p '.' in
-      v := int_of_string (String.sub s !p (i - !p)) :: !v;
-      p := i + 1
-    done with Not_found | Failure "int_of_string" -> () end;
-    List.rev !v, String.sub s !p (String.length s - !p)
+    let rec first_not_digit p =
+      if p < String.length s then
+	match s.[p] with
+	  | '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '.' ->
+	      first_not_digit (p + 1)
+	  | _ -> p
+      else raise Not_found
+    in
+    let num_part, suffix_part =
+      try
+	let i = first_not_digit 0 in
+	String.sub s 0 i, String.sub s i (String.length s - i)
+      with Not_found -> s, ""
+    in
+    let rec split s p =
+      try
+	let i = String.index_from s p '.' in
+	String.sub s p (i-p) :: (split s (i+1))
+      with Not_found ->
+	if s = "" then [] else [String.sub s p (String.length s - p)]
+    in
+    List.map int_of_string (split num_part 0), suffix_part
 
   let to_string (v, s) =
     String.concat "." (List.map string_of_int v) ^ s
@@ -30,36 +45,79 @@ end = struct
     String.length s2 >= l1 &&
       String.sub s2 0 l1 = s1
 
+  let rec compare_vnums a b = match a, b with
+    | [], [] -> 0
+    | [], _ -> -1
+    | _, [] -> 1
+    | x1::r1, x2::r2 ->
+	match x1 - x2 with
+	  | 0 -> compare_vnums r1 r2
+	  | c -> c
+
   let compare (v1, s1) (v2, s2) =
-    match List.fold_left2 begin function
-      | 0 -> compare
-      | c -> fun _ _ -> c
-    end 0 v1 v2 with
+    match compare_vnums v1 v2 with
       | 0 ->
-          begin match prefix s1 s2, prefix s2 s1 with
-            | true, true -> 0
-            | true, false -> -1
-            | false, true -> 1
-            | false, false ->
-                raise (Uncomparable_versions((v1, s1), (v2, s2)))
-          end
+	  begin match prefix s1 s2, prefix s2 s1 with
+	    | true, true -> 0
+	    | true, false -> -1
+	    | false, true -> 1
+	    | false, false ->
+		raise (Uncomparable_versions(([], s1), ([], s2)))
+	  end
       | c -> c
+
+  let max v1 v2 = match compare v1 v2 with
+    | 1 -> v2
+    | _ -> v1
 end
 
-type executable = {
-  name: string;
-  version: Version.t;
-}
+let best _ = assert false
 
-let var x y = printf "%s = %s\n" x y
-(*******************************************************************************)
+let query ?(default = "") question = assert false
 
-(*let () =
-  var "OCAMLC" ocamlc.name;
-  var "OCAMLOPT" ocamlopt.name;
-  var "OCAMLLEX" ocamllex.name;
-  var "OCAMLYACC" ocamlyacc.name;
-  var "OCAMLDOC" ocamldoc.name;
+let require _ = assert false
+
+let exec _ _ = assert false
+
+let check_file _ = assert false
+(******************************************************************************)
+
+let () =
+  let bestopt x = best [x^".opt"; x] in
+  let ocamlc = query ?default: (bestopt "ocamlc") "OCaml compiler" in
+  check_file ocamlc;
+  let best_ocaml x = bestopt (Filename.dirname ocamlc ^ x) in
+  let ocamlopt = best_ocaml "ocamlopt" in
+  let ocamllex = require (best_ocaml "ocamllex") in
+  let ocamlyacc = require (best_ocaml "ocamlyacc") in
+  let ocamldoc = best_ocaml "ocamldoc" in
+  let bin = query ~default: "/usr/local/bin" "Install directory (programs)" in
+  let ocaml_lib = query ~default: (exec ocamlc ["-where"])
+    "Install directory (OCaml libraries)" in
+
+  let ocaml_version = Version.of_string (exec ocamlc ["-version"]) in
+
+  let out = open_out "Config" in
+  let var = fprintf out "%s = %s\n" in
+  let ovar x = function None -> () | Some y -> var x y in
+  var "OCAMLC" ocamlc;
+  ovar "OCAMLOPT" ocamlopt;
+  var "OCAMLLEX" ocamllex;
+  var "OCAMLYACC" ocamlyacc;
+  ovar "OCAMLDOC" ocamldoc;
   var "BIN" bin;
-  var "OCAMLLIB" 
-*)
+  var "OCAMLLIB" ocaml_lib;
+
+  printf "
+Summary:
+--------
+OCaml version: %s
+ocamlc: %s
+ocamlopt: %s
+ocamllex: %s
+ocamlyacc: %s
+ocamldoc: %s
+Install directory (programs): %s
+Install directory (OCaml libraries): %s"
+    (Version.to_string ocaml_version) ocamlc ocamlopt ocamllex ocamlyacc
+    ocamldoc bin ocaml_lib
