d36a0a0667d8c216200965cea3844504d69d0e3b
darcs <darcs@doomeer.com>
2009-02-01 13:42:54 +0000

    version: added release

--- version.ml
+++ version.ml
@@ -1,5 +1,7 @@
 let major = 1
 let minor = 0
-let full = string_of_int major ^ "." ^ string_of_int minor
+let release = 0
+let full =
+  string_of_int major ^ "." ^ string_of_int minor ^ "." ^ string_of_int release
 
 let print () = Printf.printf "%s\n%!" full
